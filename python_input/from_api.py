667d3804205b6066d1993ed3ca259bdd5b9e18c3
Xarang <victor.coatalem@gmail.com>
2023-03-22 05:51:18 +0100

    add input treatment file

---
+++ python_input/from_api.py
@@ -0,0 +1,15 @@
+import csv
+import urllib.request
+
+
+def get_data() -> list[list[str]]:
+    url = "https://exaltemps-challenge-algo.s3.eu-west-3.amazonaws.com/easy.csv"
+
+    with urllib.request.urlopen(url) as response:
+        csv_data = response.read().decode('utf-8').splitlines()
+
+    data = []
+    reader = csv.reader(csv_data)
+    for row in reader:
+        data.append(row)
+    return data
